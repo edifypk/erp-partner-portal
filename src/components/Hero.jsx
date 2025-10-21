4e314f935e3903a784a668538fbe691986e08987
Ghulam Qadir <connecttoqadir@gmail.com>
2025-10-22 04:04:19 +0500

    little design fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div className='w-full h-screen relative'>
+        <div style={{ height: "100dvh" }} className='w-full h-screen relative'>
 
 
             <video
                 src="https://erp-assets.edify.pk/videos/10200648-uhd_3840_2160_25fps%20(1)%20(1).mp4"
                 autoPlay
                 muted
+                playsInline
                 loop
                 className='w-full h-full object-cover'
                 poster='/images/hero-poster.jpg'
@@ -20,8 +21,8 @@ const Hero = () => {
             <div className='absolute inset-0 p-6 bg-linear-to-r from-black/30 to-transparent'>
                 <div className='max-w-7xl mx-auto h-full flex flex-col justify-center'>
                     <div className='max-w-2xl'>
-                        <h1 className='text-5xl text-shadow-lg/30 tracking-tighter font-semibold text-white'>Power your success <br /> with Edify Group</h1>
-                        <p className='font-medium text-shadow-lg/50 tracking-normal my-6 text-white'>Experience seamless global student recruitment through trusted partnerships, advanced technology, and dedicated support that helps your business grow.</p>
+                        <h1 className='text-3xl md:text-4xl lg:text-5xl text-shadow-lg/30 tracking-tighter font-semibold text-white'>Power your success <br /> with Edify Group</h1>
+                        <p className='font-medium text-shadow-lg/50 tracking-normal mt-4 mb-8 text-white'>Experience seamless global student recruitment through trusted partnerships, advanced technology, and dedicated support that helps your business grow.</p>
 
                         <div className='flex'>
                             <Link href="/" className='p-3 tracking-tight pl-6 font-medium rounded-full flex justify-between bg-white/70 backdrop-blur-sm items-center min-w-[250px]'>
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -13,15 +13,15 @@ const Navbar = () => {
 
       <nav className='max-w-7xl mx-auto flex justify-between items-center rounded-full py-4 pl-6 pr-4 bg-linear-to-r from-white/50 to-white/80 backdrop-blur-sm'>
         <div>
-          <img src="https://edify.pk/images/logo.svg" alt="" />
+          <img className='w-32 md:w-36' src="https://edify.pk/images/logo.svg" alt="" />
         </div>
         <div>
 
         </div>
 
         <div className='flex gap-2'>
-          <Link className='px-6 py-3 border border-blue-600 text-sm font-semibold bg-blue-600 text-white rounded-full' href="/">Register</Link>
-          <Link className='px-6 py-3 border border-blue-600 text-sm font-semibold bg-white text-blue-600 rounded-full' href="/">Login</Link>
+          <Link className='px-3 md:px-6 py-2 md:py-3 border border-blue-600 text-sm font-semibold bg-blue-600 text-white rounded-full' href="/">Register</Link>
+          <Link className='px-3 hidden sm:block md:px-6 py-2 md:py-3 border border-blue-600 text-sm font-semibold bg-white text-blue-600 rounded-full' href="/">Login</Link>
         </div>
       </nav>
 
